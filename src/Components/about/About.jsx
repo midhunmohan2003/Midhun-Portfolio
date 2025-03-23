3251f3cae9be660f6be8c22e14e0791301905cf4
midhunmohan2003 <midhunmohan2215@gmail.com>
2025-03-23 13:49:13 +0530

    updated

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './about.css'
 import AboutImg from '../../assets/about.jpg'
-import CV from '../../assets/Midhun Mohan-Resume.pdf'
+import CV from '../../assets/Midhun Mohan_Resume.pdf'
 import Info from './Info'
 
 function About() {
